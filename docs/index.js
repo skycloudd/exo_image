9f695aee2d8e03bb293bdcf07a48ba557fd96bb0
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2023-03-29 18:17:43 +0200

    docs directory

---
+++ docs/index.js
@@ -0,0 +1,23 @@
+import init, { convert } from "./exo_image.js";
+
+init();
+
+document.getElementById("generate-button").addEventListener("click", function runConvert() {
+    var f = document.getElementById("image-upload").files[0];
+    var r = new FileReader();
+
+    r.onloadend = function (e) {
+        var result = convert(e.target.result);
+
+        var blob = new Blob([result], { type: "application/octet-stream" });
+
+        var a = document.getElementById("download-a");
+        a.href = window.URL.createObjectURL(blob);
+        a.download = "exoimage-" + Date.now() + ".exolvl";
+
+        var button = document.getElementById("download-button");
+        button.disabled = false;
+    }
+
+    r.readAsDataURL(f);
+}, false);
